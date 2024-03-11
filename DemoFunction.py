6859bdb7c79a45c2a5a5313e219cbe9e1e41c4a6
sjlee2744 <seongjoon.lee@gmail.com>
2024-03-11 14:53:19 +0900

    함수 연습2

--- DemoFunction.py
+++ DemoFunction.py
@@ -7,4 +7,33 @@ def setValue(newValue):
 
 #호촐
 print(setValue(5))
-print(setValue(5))
+
+# 튜풀로 리턴
+def swap(x, y):
+    return y, x
+
+#호출
+print(swap(3,4))
+
+#지역변수 전역변수
+x = 10
+def func(a):
+    return a + x
+
+#  호출
+print(func(1))
+
+def func2(a):
+    x = 5
+    return a + x
+# 호출
+print(func2(1))
+
+#기본값 세팅
+def times(a=10, b=20):
+    return a*b
+
+print(times())
+print(times(5))
+print(times(5,6))
+
