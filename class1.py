09df6c6a1e635755327d3aefa893535eb5796688
sjlee2744 <seongjoon.lee@gmail.com>
2024-03-12 10:38:21 +0900

    클래스 연습 완료

--- class1.py
+++ class1.py
@@ -7,8 +7,12 @@ class Person:
     def __init__ (self):
         self.name = "default name"
     def print(self):
-        peint("My name is {0}" .format(self.name))
+        print("My name is {0}" .format(self.name))
 
 # 인스턴스 생성
 p1 = Person()
+p2 = Person()
+p1.name = "전우치"
 p1.print()
+p2.print()
+
